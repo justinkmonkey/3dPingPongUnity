9488db51aa158e6f880084278c70af60f427018d
umair <eeumairali@gmail.com>
2026-01-03 22:56:21 +0500

    keys are not working with ball

--- Assets/move_ball.cs
+++ Assets/move_ball.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class move_ball : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+    //  use input from arrow keys
+    //  if w is pressed, move up
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
     }
 }
