c0aceb5dea3615139b472dfbb5ad3e915b4a9b80
umair <eeumairali@gmail.com>
2026-01-12 20:24:04 +0500

    dsa

--- Assets/move_ball.cs
+++ Assets/move_ball.cs
@@ -2,27 +2,25 @@ using UnityEngine;
 
 public class move_ball : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 6f;
+    Rigidbody rb;
 
-    void Update()
+    void Start()
     {
-    //  use input from arrow keys
-    //  if w is pressed, move up
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.up * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.down * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float x = 0f;
+        float z = 0f;
+
+        if (Input.GetKey(KeyCode.W)) z = 1;
+        if (Input.GetKey(KeyCode.S)) z = -1;
+        if (Input.GetKey(KeyCode.A)) x = -1;
+        if (Input.GetKey(KeyCode.D)) x = 1;
+
+        Vector3 move = new Vector3(x, 0, z) * speed;
+        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
     }
 }
